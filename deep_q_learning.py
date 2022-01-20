569f9931cedb5b986ea416380e5e7116d030c24c
Jean-Baptiste Gay <41980649+j6baptiste@users.noreply.github.com>
2022-01-20 04:17:10 -0800

    Create
    add sources

---
+++ deep_q_learning.py
@@ -0,0 +1,7 @@
+'''
+Sources:
+ - https://towardsdatascience.com/a-minimal-working-example-for-deep-q-learning-in-tensorflow-2-0-e0ca8a944d5e
+   (Code: https://github.com/woutervanheeswijk/example_deep_q_learning/blob/main/main.py)
+ - https://towardsdatascience.com/deep-q-learning-tutorial-mindqn-2a4c855abffc
+   (Code: https://github.com/mswang12/minDQN/blob/main/minDQN.py)
+'''
